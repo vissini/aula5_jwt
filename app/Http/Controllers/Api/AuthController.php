624baf1a7060df7a78aabfe942e3b7358f08b790
Diego Vissini <diego@vissini.com.br>
2019-08-04 09:46:17 -0300

    API Rest com JWT

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+class AuthController extends Controller
+{
+    use AuthenticatesUsers;
+    
+    public function refresh(){
+        $token = \Auth::guard('api')->refresh();
+        return ['token' => $token]; //No-content
+    }
+    
+    public function logout(){
+        \Auth::guard('api')->logout();
+        return response()->json([],204); //No-content
+    }
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+        $credentials = $this->credentials($request);
+        $token = \JWTAuth::attempt($credentials);
+        return $this->responseToken($token);
+    }
+    private function responseToken($token)
+    {
+        return $token ? ['token' => $token] :
+            response()->json([
+                'error' => \Lang::get('auth.failed')
+            ], 400);
+    }
+
+}
